88d5249b4c25ae7bd782fe645cb53b9bb9831456
Varun Goyal <42479771+govarun@users.noreply.github.com>
2019-02-01 01:39:38 +0530

    Update hello-world.sh

--- hello-world.sh
+++ hello-world.sh
@@ -1,1 +1,9 @@
+# nice source - https://www.taniarascia.com/how-to-create-and-use-bash-scripts/
+
 #!/bin/bash
+# The above line tells that this script should run with bash rather than any other scripting language.
+
+echo Hello, World!
+# Strings can be written directly, or within single quotes or double quotes. 
+# A single quoted string will not interpolate variables, but a double quoted string will.
+
